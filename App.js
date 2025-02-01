92074d5340804a7bda8ec377dd4278c9f6750c57
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-02-01 11:13:23 +0530

    justifycontent,align items tutorials

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import React from "react";
-import {View,SafeAreaView } from "react-native";
+import {View } from "react-native";
 
 
 
 export default function App() {
   console.log("App executed");
   return (
-    <SafeAreaView>
-
-    <View style={{backgroundColor: "dodgerblue", flex: 1,flexDirection: "row"}}>
-      <View style={{backgroundColor: "gold",width:100,height:100}}/>
-      <View style={{backgroundColor: "tomato", width:100,height:100}}/>
-      <View style={{backgroundColor: "dodgerblue", width:100,height:100}}/>
+    
+      <View style={{
+        backgroundColor: "dodgerblue",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center", // main axis
+        alignItems:"center" // cross axis
+      }}>
+      <View style={{
+        backgroundColor: "gold",
+        width: 100,
+        height: 300,
+        alignSelf:"flex-start"
+      }} />
+      <View style={{
+        backgroundColor: "tomato",
+        width: 100,
+        height: 200
+      }} />
+      <View style={{
+        backgroundColor: "blue",
+        width: 100,
+        height: 100
+      }} />
       
     </View>
-    </SafeAreaView>
   );
 }
 
