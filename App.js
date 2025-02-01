a0a18e4fbd1c77aecde1bac584056c57df6cbef4
denuwanhendalage <shashankadenuwanb@gmail.com>
2025-02-01 11:19:05 +0530

    flexWrap. alignContent

--- App.js
+++ App.js
@@ -12,21 +12,33 @@ export default function App() {
         flex: 1,
         flexDirection: "row",
         justifyContent: "center", // main axis
-        alignItems:"center" // cross axis
+      alignItems: "center", // cross axis
+        alignContent:"center", //align entire content
+        flexWrap:"wrap"
+        
       }}>
       <View style={{
         backgroundColor: "gold",
         width: 100,
-        height: 300,
-        alignSelf:"flex-start"
+        height: 300
       }} />
       <View style={{
         backgroundColor: "tomato",
         width: 100,
-        height: 200
+        height: 100
       }} />
       <View style={{
         backgroundColor: "blue",
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: "grey",
+        width: 100,
+        height: 100
+      }} />
+      <View style={{
+        backgroundColor: "green",
         width: 100,
         height: 100
       }} />
